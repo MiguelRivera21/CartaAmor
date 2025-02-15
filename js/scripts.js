e7f38742bb07574d07556c53ccc2039c21ba2011
MiguelRivera21 <21miguelrivera@micorreo.upp.edu.mx>
2025-02-14 20:36:23 -0600

    index

---
+++ js/scripts.js
@@ -0,0 +1,4 @@
+// Puedes agregar interactividad aquí si lo deseas, como una animación o mensaje especial
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("La carta de amor está lista para ser leída.");
+});
